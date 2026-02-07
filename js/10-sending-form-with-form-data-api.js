1fb1c1f4df14892b43fb7210e5cfb6e7d9dccf06
luis <luisdavidguevam20041017@gmail.com>
2026-02-07 13:20:42 -0500

    feat: nuevas modifcaciones en el archivo 10

--- js/10-sending-form-with-form-data-api.js
+++ js/10-sending-form-with-form-data-api.js
@@ -2,10 +2,38 @@
 
 const userForm = document.querySelector(".formulario");
 
+
 userForm.addEventListener("submit", function(e){
 
     e.preventDefault();
     
+    const data = new  FormData(userForm);
+    const userData = Object.fromEntries(data.entries())
+
+    console.log("Todos los datos del usuario son:", userData);
+
+    // console.log("Este es el nombre del usuario: ",userData.userName)
+
+
+    // Destructuring
+
+    const { userName }= userData;
+        console.log(userName)
+
+
+
+    // console.log("Todos los datos del usuario son:", userPhone);
+
+    // const{userPhone} = FormData
+
+    // console.log("Este es el telefono: ",userData["user-phone"]);
+    // console.log("Este es el telefono: ",userData["user-name"]);
+
+    // console.log("Nombre:", userData["user-name"], " Teléfono:", userData["user-phone"]);
+
+    // const tel = userData["user-phone"];
+
+    // console.log("Este es una variable", tel);
 
 });
 
